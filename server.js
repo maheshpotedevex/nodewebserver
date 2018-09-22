b56f15f83fb697b2c4899eb61e22e9d4bcf3e457
maheshpote <maheshpote.inmac@gmail.com>
2018-09-22 11:16:07 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+// User commen use file to all pages.
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+// More about middleware; Keep track how are server working.
+app.use((req, res, next) => {
+    let now = new Date().toString(); // toString shown human readable text formet.
+    let log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        // if (err) throw err;  OR
+        if (err) {
+            console.log("Unable to append to server.log");
+        }
+    });
+    next();
+});
+
+// Maintenance middleware
+/*app.use((req, res, next) => {
+                res.render('maintenance.hbs');
+            });
+*/
+// MIddleware
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+// 1. http get request
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: "Starting page - Home page.",
+        welcomeMessage: "Welcome to my new website"
+    });
+});
+// 2.
+app.get('/about', (req, res) => {
+    //res.send("About Page.");
+    res.render('about.hbs', {
+        pageTitle: "About Page123"
+    });
+});
+// 3. 
+app.get('/bad', (req, res) => {
+    res.send({
+        erroMessage: "Unable to handle request!"
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server is up on the port 3000");
+});
